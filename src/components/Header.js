87c4216f31422d1d8d1c16247927862176ea5c62
anmolb-cogo <anmol.bansal@cogoport.com>
2023-04-05 17:04:30 +0530

    added mapping

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,51 @@
-import { IcMInstagramLogo, IcMLinkedinLogo, IcMTwitter } from '@cogoport/icons-react'
+/* eslint-disable camelcase */
+import {
+  IcMInstagramLogo,
+  IcMLinkedinLogo,
+  IcMTwitter
+} from '@cogoport/icons-react'
 import styles from './styles.module.css'
+import { HEADER } from '../content'
 
 function Header () {
+  const {
+    profile_url = '',
+    profile_alt = '',
+    heading = '',
+    sub_heading = '',
+    links = {}
+  } = HEADER
+
+  const width = 30
+  const height = 30
+
+  const LINKS_MAPPING = {
+    instagram: <IcMInstagramLogo width={width} height={height} />,
+    linkedin: <IcMLinkedinLogo width={width} height={height} />,
+    twitter: <IcMTwitter width={width} height={height} />
+  }
+
   return (
-    <div className={styles.header}>
-      <div className={styles.header_left}>
-       <div className={styles.avatar}></div>
-            <div className={styles.title_container}>
-              <div className={styles.title}>Anmol Bansal </div>
-              <div className={styles.subtitle}>Software Engineer | Some Content</div>
+        <div className={styles.header}>
+            <div className={styles.header_left}>
+                <div className={styles.avatar}>
+                    <img src={profile_url} alt={profile_alt}></img>
+                </div>
+
+                <div className={styles.title_container}>
+                    <div className={styles.title}>{heading}</div>
+                    <div className={styles.subtitle}>{sub_heading}</div>
+                </div>
             </div>
-      </div>
 
             <div className={styles.social}>
-                <IcMInstagramLogo height={32} width={32}/>
-                <IcMTwitter height={32} width={32}/>
-                <IcMLinkedinLogo height={32} width={32}/>
+                {Object.keys(links).map((key, index) => (
+                    <a href={links[key]} alt={key} key={key}>
+                        {LINKS_MAPPING[key]}
+                    </a>
+                ))}
             </div>
-    </div>
+        </div>
   )
 }
 
---
+++ src/content.js
@@ -0,0 +1,54 @@
+export const HEADER = {
+  profile_url: 'https://image.typedream.net/cdn-cgi/image/width=3840,quality=100/https://api.typedream.com/v0/document/public/8bd6caf2-6ea7-4a61-8ed7-ebeead4e5580/2NfjYzn4a3OFA12xkx38eHPqPYt_Profile.jpeg',
+  profile_alt: 'Profile Image',
+  heading: 'Anmol Bansal | Software Developer',
+  sub_heading: 'SDE at Cogoport, Mumbai',
+  links: {
+    instagram: 'something',
+    twitter: 'something twi',
+    linkedin: 'some link'
+  }
+}
+
+export const LONG_CARDS = [
+  {
+    id: 1,
+    title: 'Some Long Content 1',
+    button_label: 'Button Label 1',
+    button_url: '',
+    image: '',
+    video: ''
+  },
+  {
+    id: 2,
+    title: 'Some Long Content 2',
+    button_label: 'Button Label 2',
+    button_url: '',
+    image: '',
+    video: ''
+  }
+]
+
+export const SHORT_CARDS = [
+  {
+    id: 1,
+    title: 'Some Long Content 1',
+    button_label: 'Button Label 1',
+    button_url: '',
+    image: '',
+    video: ''
+  },
+  {
+    id: 2,
+    title: 'Some Long Content 2',
+    button_label: 'Button Label 2',
+    button_url: '',
+    image: '',
+    video: ''
+  }
+]
+
+export const FOOTER = {
+  content: '@anmollbansall',
+  url: '#'
+}
